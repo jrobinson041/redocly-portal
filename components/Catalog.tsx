830b194f208683535fc63d25faebba27bbf81d70
John Robinson <john.robinson@canoo.com>
2022-09-30 13:14:57 -0400

    Add catalog

---
+++ components/Catalog.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+import {
+  useCatalog,
+  FlexSection,
+  Flex,
+  WideTile,
+  SectionHeader,
+  LoadingAnimation,
+  H2,
+} from "@redocly/developer-portal/ui";
+
+export function Catalog() {
+  const { apis, loadingRbac } = useCatalog({ offset: 0, limit: 10 });
+
+  if (!apis.length) {
+    return <H2>You don't have access to any APIs.</H2>;
+  }
+
+  return (
+    <>
+      <Flex flexDirection="row" alignItems="baseline">
+        <SectionHeader> API Catalog</SectionHeader>
+        {loadingRbac ? <LoadingAnimation size={20} /> : null}
+      </Flex>
+      <FlexSection justifyContent="space-around" flexWrap="wrap">
+        {apis.map((api) => (
+          <WideTile to={api.link} header={api.title || api.link}>
+            Tags:{" "}
+            {api.defaultVersion.metadata?.tags.map((tag) => (
+              <span> {tag} </span>
+            ))}
+          </WideTile>
+        ))}
+      </FlexSection>
+    </>
+  );
+}
--- theme.ts
+++ theme.ts
@@ -1,4 +1,4 @@
-import { lighten, darken, readableColor } from 'polished';
+import { lighten, darken, readableColor } from "polished";
 
 export const theme = {
   // spacing: {
@@ -15,7 +15,7 @@ export const theme = {
   colors: {
     // tonalOffset: 0.2,
     primary: {
-      main: '#227a88',
+      main: "#222222",
       // light: ({ colors }) => lighten(colors.tonalOffset, colors.primary.main),
       // dark: ({ colors }) => darken(colors.tonalOffset, colors.primary.main),
       // contrastText: ({ colors }) => readableColor(colors.primary.main),
@@ -45,7 +45,7 @@ export const theme = {
     //   contrastText: ({ colors }) => readableColor(colors.info.main),
     // },
     text: {
-      primary: '#424242',
+      primary: "#444444",
       // secondary: '#4e566d',
     },
     // border: {
@@ -71,15 +71,15 @@ export const theme = {
     //   },
     // },
     http: {
-      get: '#6bbd5b',
-      post: '#248fb2',
-      put: '#9b708b',
-      options: '#d3ca12',
-      patch: '#e09d43',
-      delete: '#e27a7a',
-      basic: '#999',
-      link: '#31bbb6',
-      head: '#c167e4',
+      get: "#6bbd5b",
+      post: "#248fb2",
+      put: "#9b708b",
+      options: "#d3ca12",
+      patch: "#e09d43",
+      delete: "#e27a7a",
+      basic: "#999",
+      link: "#31bbb6",
+      head: "#c167e4",
     },
     // navbar: {
     //   main: ({ colors }) => colors.primary.main,
@@ -87,49 +87,49 @@ export const theme = {
     //   contrastText: 'white'
     // },
     // footer: {
-      // main: ({ colors }) => colors.primary.main,
-      // contrastText: 'white'
+    // main: ({ colors }) => colors.primary.main,
+    // contrastText: 'white'
     // },
   },
 
   sidebar: {
-    backgroundColor: '#fafafa',
-    width: '260px',
+    backgroundColor: "#fafafa",
+    width: "260px",
   },
   // tocPanel: {
   //   width: '240px',
   // },
 
   typography: {
-    fontSize: '16px',
-    lineHeight: '1.5em',
-    fontWeightRegular: '400',
-    fontWeightBold: '600',
-    fontWeightLight: '300',
-    fontFamily: '"Source Sans Pro", sans-serif',
+    fontSize: "16px",
+    lineHeight: "1.5em",
+    fontWeightRegular: "400",
+    fontWeightBold: "600",
+    fontWeightLight: "300",
+    fontFamily: '"AkkuratLLWeb-Regular", sans-serif',
     headings: {
-      fontFamily: '"Source Sans Pro", sans-serif',
-      fontWeight: '600',
+      fontFamily: '"AkkuratMonoLLWeb-Regular", sans-serif',
+      fontWeight: "300",
     },
     // heading1: {
-    //   fontSize: '1.85714em',
-    //   fontWeight: '600',
+    //   fontSize: "1.85714em",
+    //   fontWeight: "200",
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
     //   color: ({ colors }) => colors.primary.main,
     //   capitalize: true,
     // },
     // heading2: {
-    //   fontSize: '1.57143em',
-    //   fontWeight: '600',
+    //   fontSize: "1.57143em",
+    //   fontWeight: "200",
     //   color: ({ colors }) => colors.text.primary,
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
     //   capitalize: false,
     // },
     // heading3: {
-    //   fontSize: '1.27em',
-    //   fontWeight: '600',
+    //   fontSize: "1.27em",
+    //   fontWeight: "600",
     //   color: ({ colors }) => colors.text.primary,
     //   fontFamily: ({ typography }) => typography.headings.fontFamily,
     //   lineHeight: ({ typography }) => typography.lineHeight,
@@ -145,11 +145,11 @@ export const theme = {
     // // ...
     // },
     code: {
-      fontSize: '13px',
-      fontFamily: '"Source Code Pro", sans-serif',
+      fontSize: "13px",
+      fontFamily: '"AkkuratMonoLLWeb-Regular", sans-serif',
       // fontWeight: ({ typography }) => typography.fontWeightRegular,
-      color: '#e53935',
-      backgroundColor: 'rgba(38, 50, 56, 0.04)',
+      color: "#e53935",
+      backgroundColor: "rgba(38, 50, 56, 0.04)",
       wrap: false,
     },
     links: {
@@ -159,12 +159,12 @@ export const theme = {
     },
   },
   rightPanel: {
-    backgroundColor: '#263238',
-    width: '40%',
+    backgroundColor: "#263238",
+    width: "40%",
     // textColor: '#ffffff',
   },
   schema: {
-    nestedBackground: '#fafafa',
+    nestedBackground: "#fafafa",
     // linesColor: theme => lighten( theme.colors.tonalOffset, desaturate(theme.colors.tonalOffset, theme.colors.primary.main) ),
     // defaultDetailsWidth: '75%',
     // typeNameColor: theme => theme.colors.text.secondary,
